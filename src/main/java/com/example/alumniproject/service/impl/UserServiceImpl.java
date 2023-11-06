48860d624e7babd6f32349d79594f6df4af5899e
Dashzevge <76143002+Dashzevge@users.noreply.github.com>
2023-11-06 15:58:18 -0600

    Add files via upload

--- src/main/java/com/example/alumniproject/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/alumniproject/service/impl/UserServiceImpl.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+import java.time.Duration;
 import java.util.List;
 import java.util.Optional;
 
@@ -29,5 +31,37 @@ public class UserServiceImpl implements UserService {
     @Override
     public Optional<User> findById(Integer id) {
         return repository.findById(id);
+    }
+
+    @Override
+    public Optional<User> findByFirstName(String username){
+        return repository.findByFirstName(username);
+    }
+
+    @Override
+    public boolean isUserLockedOut(User user){
+        LocalDateTime  lastFailedLogin = user.getLastFailedLoginTimestamp();
+        if(lastFailedLogin != null)
+        {
+            LocalDateTime currentTime = LocalDateTime.now();
+            long minDifference = Duration.between(lastFailedLogin, currentTime).toMinutes();
+
+            if(minDifference < 15 && (user.getAccountLocked() != null &&  user.getAccountLocked())){
+                return true;
+            }
+            else {
+                user.setAccountLocked(false);
+                user.setLastFailedLoginTimestamp(null);
+                repository.save(user);
+                return false;
+            }
+        }
+        return false;
+    }
+    @Override
+    public void lockUserAccount(User user){
+        user.setAccountLocked(true);
+        user.setLastFailedLoginTimestamp(LocalDateTime.now());
+        repository.save(user);
     }
 }
